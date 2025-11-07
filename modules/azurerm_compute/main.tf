8682663e53cf5291da25940dd775a0a1c83a9904
Chandra Shekhar <chandra.sahni670@gmail.com>
2025-11-07 15:13:49 +0530

    Added NSG and its association

--- modules/azurerm_compute/main.tf
+++ modules/azurerm_compute/main.tf
@@ -1,3 +1,52 @@
+resource "azurerm_network_security_group" "nsgs" {
+  for_each = var.vms
+
+  name                = "${each.value.vm_name}-nsg"
+  location            = each.value.location
+  resource_group_name = each.value.rg_name
+
+  # Example: Allow SSH access
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Example: Allow HTTP access
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Example: Allow HTTPS access
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = lookup(each.value, "tags", null)
+}
+
 resource "azurerm_network_interface" "nic" {
   for_each            = var.vms
   name                = each.value.nic_name
@@ -10,7 +59,13 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = data.azurerm_public_ip.pip[each.key].id
   }
+
   tags = lookup(each.value, "tags", null)
+}
+resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+  for_each                     = var.vms
+  network_interface_id         = azurerm_network_interface.nic[each.key].id
+  network_security_group_id = azurerm_network_security_group.nsgs[each.key].id
 }
 resource "azurerm_linux_virtual_machine" "vm" {
   for_each                        = var.vms
--- modules/azurerm_compute/terraform.tfvars
+++ modules/azurerm_compute/terraform.tfvars
@@ -6,8 +6,8 @@ vms = {
     vnet_name      = "vnet-pillu-dev-todoapp-01"
     subnet_name    = "frontend-subnet"
     pip_name       = "pip-pillu-dev-todoapp-01"
-    vm_name        = "pillu-vm"
-    size           = "Standard_B2s"
+    vm_name        = "pillu-vm1"
+    size           = "Standard_L2aos_v4"
     admin_username = "devopsadmin"
     admin_password = "P@ssword1234"
     source_image_reference = {
@@ -18,23 +18,22 @@ vms = {
     }
 
   }
-  vm2 = {
-    nic_name       = "nic-backend-vm-01"
-    location       = "eastus"
-    rg_name        = "rg-pillu-dev-todoapp-01"
-    vnet_name      = "vnet-pillu-dev-todoapp-01"
-    subnet_name    = "frontend-subnet"
-    pip_name       = "pip-pillu-dev-todoapp-02"
-    vm_name        = "pillu-vm"
-    size           = "Standard_B2s"
-    admin_username = "adminuser"
-    admin_password = "P@ssword1234"
-    source_image_reference = {
-      publisher = "Canonical"
-      offer     = "0001-com-ubuntu-server-jammy"
-      sku       = "22_04-lts-gen2"
-      version   = "latest"
-    }
-
-  }
+  # vm2 = {
+  #   nic_name       = "nic-backend-vm-01"
+  #   location       = "eastus"
+  #   rg_name        = "rg-pillu-dev-todoapp-01"
+  #   vnet_name      = "vnet-pillu-dev-todoapp-01"
+  #   subnet_name    = "frontend-subnet"
+  #   pip_name       = "pip-pillu-dev-todoapp-02"
+  #   vm_name        = "pillu-vm2"
+  #   size           = "Standard_L2aos_v4"
+  #   admin_username = "adminuser"
+  #   admin_password = "P@ssword1234"
+  #   source_image_reference = {
+  #     publisher = "Canonical"
+  #     offer     = "0001-com-ubuntu-server-jammy"
+  #     sku       = "22_04-lts-gen2"
+  #     version   = "latest"
+  #   }
+  # }
 }
