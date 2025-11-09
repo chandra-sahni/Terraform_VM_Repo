2de7e1956f170069d9659f2bae35c8e3aa1d0cde
Chandra Shekhar <chandra.sahni670@gmail.com>
2025-11-09 16:03:43 +0530

    Keyvault integrated for vm login security

--- modules/azurerm_compute/main.tf
+++ modules/azurerm_compute/main.tf
@@ -67,14 +67,31 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" 
   network_interface_id      = azurerm_network_interface.nic[each.key].id
   network_security_group_id = azurerm_network_security_group.nsgs[each.key].id
 }
+data "azurerm_key_vault" "kv" {
+  for_each            = var.vms
+  name                = each.value.kv_name
+  resource_group_name = each.value.rg_name
+}
+data "azurerm_key_vault_secret" "vm_username" {
+  for_each = var.vms
+
+  name         = "vm-username"
+  key_vault_id = data.azurerm_key_vault.kv[each.key].id
+}
+data "azurerm_key_vault_secret" "vm_password" {
+  for_each = var.vms
+
+  name         = "vm-password"
+  key_vault_id = data.azurerm_key_vault.kv[each.key].id
+}
 resource "azurerm_linux_virtual_machine" "vm" {
   for_each                        = var.vms
   name                            = each.value.vm_name
   resource_group_name             = each.value.rg_name
   location                        = each.value.location
   size                            = each.value.size
-  admin_username                  = each.value.admin_username
-  admin_password                  = each.value.admin_password
+  admin_username                  = data.azurerm_key_vault_secret[each.key].vm_username
+  admin_password                  = data.azurerm_key_vault_secret[each.key].vm_password
   disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.nic[each.key].id
